4b08d54737c7cc04cad07438792a491e47d00b7a
MCCSharp <154324394+MCCSharp@users.noreply.github.com>
2023-12-19 12:21:53 +0100

    WarpKernSys
    Code that simulate a Warp Core Temperature regulator:
    If temparture get to high it proc an event that ask u to insert an emergencie Code,
    if success the Core tempartur go down.

---
+++ WarpKernSys/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace WarpKernSys
+{
+    using System.Timers;
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WarpKern warpKern = new WarpKern();
+            WarpKonsole warpKonsole = new();
+
+            warpKern.WarperaturKontrollerEvent += warpKonsole.WarpTemperaturKontrolle;
+            warpKern.OnWorkingProcess(warpKern);
+        }
+    }
+}
---
+++ WarpKernSys/WarpKonsole.cs
@@ -0,0 +1,51 @@
+﻿namespace WarpKernSys
+{
+     class WarpKonsole
+    {
+         Thread countdownTh;
+         CancellationTokenSource cts = new();
+         CountDown countDown;
+         bool notSutdown;
+
+        public void WarpTemperaturKontrolle(object sender, WarpEventArgs e)
+        {
+            //countDown = new(5);
+            //countdownTh = new(() => countDown.StartTimer(cts));
+            Random rnd = new Random();
+            var notPwd = rnd.Next(100000, 999999);
+            string tempInput=string.Empty;
+            notSutdown = true;
+
+            int input = 0;
+            
+            Console.WriteLine($" Achtung Temperatur ist zu hoch. Die tampeartur ist {e.WarpKern!.WarpTemperatur} Grad!");
+            Console.WriteLine($" Bitte NotfallKode Eingeben {notPwd}");
+            //countdownTh.Start();    
+            //while (countdownTh.IsAlive)
+            //{
+
+                while (input != notPwd /*&& countDown.countdown == 0*/)
+                {
+                    try
+                    {
+                        tempInput = Console.ReadLine();
+                        input = Convert.ToInt32(tempInput);
+                    }
+                    catch (InvalidCastException)
+                    {
+                        Console.WriteLine("Bitte nur Ziffer eingeben");
+                    }
+                    catch(FormatException)
+                    {
+                        Console.WriteLine("Bitte nur Ziffer eingeben");
+                    }
+                    if (input == notPwd)
+                    {
+                        e.WarpKern.WarpTemperaturZeiger(10);                 
+                    }
+                }         
+            //}
+               
+        }       
+    }
+}
